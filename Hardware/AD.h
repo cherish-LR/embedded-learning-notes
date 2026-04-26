f66840b26240b971036a8310652e8a1f2c971c40
cherish_LR <957020881@qq.com>
2026-04-26 13:47:19 +0800

    Add files via upload

---
+++ Hardware/AD.h
@@ -0,0 +1,11 @@
+#ifndef __AD_H
+#define __AD_H
+
+
+extern uint16_t AD_Valua [4];
+void AD_Init(void);
+
+
+#endif
+
+
---
+++ Hardware/LED.h
@@ -0,0 +1,10 @@
+#ifndef __LED_H
+#define __LED_H
+
+void LED_Init(void);
+void LED_1ON(void);
+void LED_1OF(void);
+void LED_2ON(void);
+void LED_2OF(void);
+
+#endif
